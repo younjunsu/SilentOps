9016b47f31766db92ddb82998026583c15ca06e4
junsu_yoon <junsu_youn@naver.com>
2024-01-15 13:51:53 +0900

    g

--- gear/scripts/so_s_listmeta.sh
+++ gear/scripts/so_s_listmeta.sh
@@ -1,14 +1,19 @@
 #!/bin/bash
+#############################################################################
 # $1:   type
 # $2:   cont_name
 # $3:   ipaddress
 # $4:   username
 # $5:   password
 # $6:   ssh port (defualt 22)
-
+#############################################################################
 export so_cont_type=`grep -w "$so_m_container" "$so_containerlistfile" |awk '{print $1}'`
 export so_cont_name=`grep -w "$so_m_container" "$so_containerlistfile" |awk '{print $2}'`
 export so_cont_ip=`grep -w "$so_m_container" "$so_containerlistfile" |awk '{print $3}'`
 export so_cont_username=`grep -w "$so_m_container" "$so_containerlistfile" |awk '{print $4}'`
 export so_cont_password=`grep -w "$so_m_container" "$so_containerlistfile" |awk '{print $5}'`
-export so_conntainersshport=`grep -w "$so_m_container" "$so_containerlistfile" |awk '{print $6}'`
+export so_cont_sshport=`grep -w "$so_m_container" "$so_containerlistfile" |awk '{print $6}'`
+if [ -z "$so_cont_sshport" ]
+then
+    export so_cont_sshport=22
+fi
--- gear/scripts/so_s_listselecttype.sh
+++ gear/scripts/so_s_listselecttype.sh
@@ -6,28 +6,28 @@ option2=$2
 
 if [ "all" = "$option2" ]
 then
-   export so_s_contlistarray=(`awk '{print $2}' $so_containerlistfile |grep -v "#"`)
+   export so_s_contlistarray=(`cat $so_containerlistfile |grep -v "#" |awk '{print $2}'`)
 elif [ "tibero" = "$option2" ]
 then
-    export so_s_contlistarray=(`awk '{print $2}' $so_containerlistfile |grep -v "#" |grep -i tibero`)
+    export so_s_contlistarray=(`cat $so_containerlistfile |grep -v "#" |grep -i tibero |awk '{print $2}'`)
 elif [ "oracle" = "$option2" ]
 then
-    export so_s_contlistarray=(`awk '{print $2}' $so_containerlistfile |grep -v "#" |grep -i oracle`)
+    export so_s_contlistarray=(`cat $so_containerlistfile |grep -v "#" |grep -i oracle |awk '{print $2}'`)
 elif [ "cubrid" = "$option2" ]
 then
-    export so_s_contlistarray=(`awk '{print $2}' $so_containerlistfile |grep -v "#" |grep -i cubrid`)
+    export so_s_contlistarray=(`cat $so_containerlistfile |grep -v "#" |grep -i cubrid |awk '{print $2}'`)
 elif [ "postgres" = "$option2" ]
 then
-    export so_s_contlistarray=(`awk '{print $2}' $so_containerlistfile |grep -v "#" |grep -i postgres`)
+    export so_s_contlistarray=(`cat $so_containerlistfile |grep -v "#" |grep -i postgres |awk '{print $2}'`)
 elif [ "mysql" = "$option2" ]
 then
-    export so_s_contlistarray=(`awk '{print $2}' $so_containerlistfile |grep -v "#" |grep -i mysql`)
+    export so_s_contlistarray=(`cat $so_containerlistfile |grep -v "#" |grep -i mysql |awk '{print $2}'`)
 elif [ "maria" = "$option2" ]
 then
-    export so_s_contlistarray=(`awk '{print $2}' $so_containerlistfile |grep -v "#" |grep -i maria`)
+    export so_s_contlistarray=(`cat $so_containerlistfile |grep -v "#" |grep -i maria |awk '{print $2}'`)
 elif [ "db2" = "$option2" ]
 then
-    export so_s_contlistarray=(`awk '{print $2}' $so_containerlistfile |grep -v "#" |grep -i db2`)
+    export so_s_contlistarray=(`cat $so_containerlistfile |grep -v "#" |grep -i db2 |awk '{print $2}'`)
 else
     exit 1
 fi

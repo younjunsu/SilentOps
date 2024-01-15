7814bf0852ec97d373d1fd5713dd252d3a67adc3
junsu_yoon <junsu_youn@naver.com>
2024-01-15 13:39:56 +0900

    Update so_s_listselecttype.sh

--- gear/scripts/so_s_listselecttype.sh
+++ gear/scripts/so_s_listselecttype.sh
@@ -5,11 +5,13 @@ option1=$1
 option2=$2
 echo "$so_containerlistfile"
 echo $option1 $option2
+
 if [ "all" = "$option2" ]
 then
    export so_s_contlistarray=(`awk '{print $3}' $so_containerlistfile |grep -v "#"`)
 elif [ "tibero" = "$option2" ]
 then
+    echo "tibero"
     export so_s_contlistarray=(`awk '{print $3}' $so_containerlistfile |grep -v "#" |grep -i tibero`)
 elif [ "oracle" = "$option2" ]
 then

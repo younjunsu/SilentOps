cb10d32e870be46b2a2189f9d8096d4028009289
junsu_yoon <junsu_youn@naver.com>
2024-01-15 10:37:38 +0900

    update

--- gear/modules/so_m_listviewmeta.sh
+++ gear/modules/so_m_listviewmeta.sh
@@ -6,6 +6,5 @@
 option1=$1
 
 sh "$so_scriptdirectory"/so_s_listselecttype.sh "$option1"
-
 for 
-printf "%10s\t\t%10s\t\t%10s\t%10s \n" "$so_containername" "$so_containerip" "$so_alivemessage" "$so_sshportmessage"
+    printf "%10s\t\t%10s\t\t%10s\t%10s \n" "$so_containername" "$so_containerip" "$so_alivemessage" "$so_sshportmessage"
